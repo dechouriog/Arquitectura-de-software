2bbbcecab7b71dcdad24177bd1f81839dd007722
jespanap <espanajaider@gmail.com>
2025-09-01 16:41:47 -0500

    features: Agregando aruqitectura MVC base y configuración base de la base de datos

--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -1,11 +1,17 @@
-from fastapi import APIRouter, Request
+from fastapi import Request
+from sqlalchemy.orm import Session
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from models.books import Book
 
 templates = Jinja2Templates(directory="views")
 
-router = APIRouter()
+def get_home(request: Request, db: Session):
+    libros = db.query(Book).limit(5).all()
+    return templates.TemplateResponse(
+        "home.html",
+        {"request": request, "libros": libros}
+    )
 
-@router.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+# Para que la ruta sepa qué tipo de respuesta usa
+get_home.response_class = HTMLResponse
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from controllers import home_controller
+from routes import home_routes
 
 app = FastAPI()
 
 # Configuración de las vistas
 templates = Jinja2Templates(directory="views")
 
-# Ruta principal delegada al controlador
-app.include_router(home_controller.router)
+# Rutas
+app.include_router(home_routes.router)
